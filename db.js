92cc1ee371fa6091a9d499ab119ba2cbcc0a7f9c
Rich Reneau <rich.reneau@gmail.com>
2018-01-11 10:59:07 -0500

    ?

--- db.js
+++ db.js
@@ -4,12 +4,42 @@ const sequelize = new Sequelize('example-blog-db','richreneau', '',{
     dialect: 'postgres'
 });
 
-sequelize
-    .authenticate()
-    .then(() => {
-        console.log('That totally connected. yeah!');
-    })
-    .catch((err) => {
-        console.error('Oh noes that did not work.');
+// sequelize
+//     .authenticate()
+//     .then(() => {
+//         console.log('That totally connected. yeah!');
+//     })
+//     .catch((err) => {
+//         console.error('Oh noes that did not work.');
+// })
+
+
+
+const User = sequelize.define('user', {
+  firstName: {
+      type: Sequelize.STRING
+  },
+  lastName: {
+      type: Sequelize.STRING
+  }    
 })
-    
+
+User.findOne({
+    where: {
+        id: 1
+    }
+})
+.then((me) => {
+    console.log(me);
+});
+
+// User.sync({ force: true})
+//     .then(() => {
+//         console.log('Table created!');
+//         return User.create({
+//             firstName: 'Rich',
+//             lastName: 'Reneau'
+//         });
+//     })
+
+module.exports = sequelize;
